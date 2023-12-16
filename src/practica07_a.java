9d1d22f4567f3c76592c8a86b059ccc987f670f5
ValCicco <valerria02@gmail.com>
2023-12-16 15:56:27 -0400

    Parte 5

--- src/practica07_a.java
+++ src/practica07_a.java
@@ -1,33 +1,57 @@
 import java.util.Scanner;
 
 public class practica07_a {
-    public static Boolean isElementOf(int[] array,int num, int z){
+    public static Boolean isElementOf(String[] array,int num, String z){
         for(int i=0 ; i<num ; i++){
-            if(array[i]==z){
-                System.out.println("Se encontro "+z+" en la posicion "+(i+1));
+            if(array[i].equals(z)){
                 return true;
             }
         }
         return false;
     }
+    public static void RellenarArreglo(String[] array, int num, Boolean relleno){
+        Scanner sc= new Scanner(System.in);
+        for(int j=0 ; j<num ; j++){
+            if(relleno){
+                System.out.println("Introduzca el numero para la posicion "+(j+1));
+                array[j]=sc.nextLine();
+            }else{
+                array[j]=null;
+            }
+            
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner sc= new Scanner(System.in);
+        Scanner rellenar=new Scanner(System.in);
+        Scanner z=new Scanner(System.in);
         System.out.println("Ingrese el tamanio del arreglo");
+        
         int num= sc.nextInt();
-        int[] array = new int[num];
-        int j=0;
-        while(j<num){
-            System.out.println("Introduzca el numero para la posicion "+(j+1));
-            array[j]=sc.nextInt();
-            j++;
-        }
-        System.out.println("Ingrese el numero que desea buscar");
-        int z=sc.nextInt();
+        String[] array = new String[num];
+        
+        System.out.println("Desea rellenar el arreglo (s/n)");
+        String rellena= rellenar.next();
+        Boolean relleno=false;
+        String s="s";
 
-        Boolean estaZ = isElementOf(array, num, z);
-    
-        if(estaZ==false){
+        if(rellena.equals(s)){
+            relleno=true;
+        }
+
+        RellenarArreglo(array, num, relleno);
+
+        System.out.println("Ingrese el numero que desea buscar");
+        
+        String Numz =z.next();
+        Boolean estaZ=false;
+        
+        if(relleno){
+            estaZ = isElementOf(array, num, Numz);
+        }
+
+        if(!estaZ){
             System.out.println("El numero NO se encuentra en el arreglo");
         }else{
             System.out.println("El numero SI esta en el arreglo");
